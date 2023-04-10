4f8544ee5c14e6d881ea9b6ee5f37297b73d63f7
nehamahajan255 <nehamahajan1902@gmail.com>
2023-04-10 10:50:28 +0530

    pdf uploader

--- admin/src/app/course-chapter/course-chapter.component.ts
+++ admin/src/app/course-chapter/course-chapter.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -18,12 +19,47 @@ export class CourseChapterComponent implements OnInit {
     duration:new FormControl("", [Validators.required]),
    
   })
-  constructor() { }
-
+  constructor(private http:HttpClient) { }
+  fileName = '';  
+videoName='';
   ngOnInit(): void {
   }
   courseFn(data:any){
     console.log(this.courseForm);
     
   }
+  onFileSelected(event:any) {  
+  
+    const file:File = event.target.files[0];  
+
+    if (file) {  
+
+        this.fileName = file.name;  
+
+        const formData = new FormData();  
+
+        formData.append("thumbnail", file);  
+
+        const upload$ = this.http.post("/api/thumbnail-upload", formData);  
+
+        upload$.subscribe();  
+    }  
+}  
+onVideoSelected(event:any) {  
+  
+  const video:File = event.target.files[0];  
+
+  if (video) {  
+
+      this.videoName = video.name;  
+
+      const formData = new FormData();  
+
+      formData.append("thumbnail", video);  
+
+      const upload$ = this.http.post("/api/thumbnail-upload", formData);  
+
+      upload$.subscribe();  
+  }  
+}  
 }
